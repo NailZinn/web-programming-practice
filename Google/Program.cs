c93a237471ef1292a81b683376a6d11bf864fa7c
Nail Zinnurov <nailzinn122033@gmail.com>
2022-09-27 20:05:26 +0300

    local sever added

---
+++ Google/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Text;
+
+namespace Google
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8888/connection/");
+            listener.Start();
+
+            Console.WriteLine("Ожидание подключений..."); 
+
+            HttpListenerContext context = await listener.GetContextAsync();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            string responseStr = await File.ReadAllTextAsync("html/index.html");
+            byte[] buffer = Encoding.UTF8.GetBytes(responseStr);
+            response.ContentLength64 = buffer.Length;
+            
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+            listener.Stop();
+           
+            Console.WriteLine("Обработка подключений завершена");
+        }
+    }
+}
