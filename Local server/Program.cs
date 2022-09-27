6478daea692ce17a6589370d6d1a31a5da98edab
Nail Zinnurov <nailzinn122033@gmail.com>
2022-09-27 21:11:06 +0300

    folder Google renamed to Local server and index.html updated

--- Local server/Program.cs
+++ Local server/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.Net;
 using System.Text;
 
-namespace Google
+namespace Local_server
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
             HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:8888/connection/");
+            listener.Prefixes.Add("http://localhost:8888/google/");
             listener.Start();
 
-            Console.WriteLine("Ожидание подключений..."); 
+            Console.WriteLine("Ожидание подключений...");
 
             HttpListenerContext context = await listener.GetContextAsync();
             HttpListenerRequest request = context.Request;
@@ -20,12 +20,12 @@ namespace Google
             string responseStr = await File.ReadAllTextAsync("html/index.html");
             byte[] buffer = Encoding.UTF8.GetBytes(responseStr);
             response.ContentLength64 = buffer.Length;
-            
+
             Stream output = response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
             output.Close();
             listener.Stop();
-           
+
             Console.WriteLine("Обработка подключений завершена");
         }
     }
