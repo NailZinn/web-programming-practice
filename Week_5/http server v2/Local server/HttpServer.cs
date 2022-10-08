b1c7ca644b26675eecb54a5320fc334c65aaed80
NailZinn <nailzinnurov17@gmail.com>
2022-10-08 14:15:57 +0300

    week_5 hw added

---
+++ Week_5/http server v2/Local server/HttpServer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Local_server
+{   
+    internal class HttpServer : IDisposable
+    {
+        private readonly HttpListener listener;
+        private ServerStatus status;
+        private ServerSettings? settings;
+
+        public ServerStatus Status => status;
+
+        public HttpServer()
+        {
+            listener = new HttpListener();
+            status = ServerStatus.Stopped;
+        }
+
+        public void Start()
+        {
+            if (status == ServerStatus.Started)
+            {
+                Console.WriteLine("Сервер уже начал свою работу");
+                return;
+            }
+
+            settings = JsonSerializer.Deserialize<ServerSettings>(File.ReadAllBytes("./settings.json"));
+
+            listener.Prefixes.Clear();
+            listener.Prefixes.Add($"http://localhost:{settings.Port}/");
+
+            Console.WriteLine("Запуск сервера...");
+            listener.Start();
+            Console.WriteLine("Сервер запущен");
+
+            status = ServerStatus.Started;
+
+            Listening();
+        }
+
+        public void Stop()
+        {
+            if (status == ServerStatus.Stopped)
+            {
+                Console.WriteLine("Сервер уже закончил свою работу");
+                return;
+            }
+
+            Console.WriteLine("Остановка сервера...");
+            listener.Stop();
+            Console.WriteLine("Сервер остановлен");
+
+            status = ServerStatus.Stopped;
+        }
+
+        private void Listening()
+        {
+            listener.BeginGetContext(new AsyncCallback(ListenerCallBack), listener);
+        }
+
+        private async void ListenerCallBack(IAsyncResult result)
+        {
+            if (listener.IsListening)
+            {
+                var context = listener.EndGetContext(result);
+                var request = context.Request;
+                using var response = context.Response;
+
+                var responseInfo = await HttpServerResponseProvider.GetResponseInfoAsync(settings.Path, request.RawUrl);
+                response.Headers.Set(HttpResponseHeader.ContentType, responseInfo.ContentType);
+                var buffer = responseInfo.Buffer;
+
+                using Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                Listening();
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
---
+++ Week_5/http server v2/Local server/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Local_server
+{
+    public class Program
+    {
+        private static bool _isRunning = true;
+
+        static void Main(string[] args)
+        {
+            using var server = new HttpServer();
+            while (_isRunning)
+            {
+                Handler(Console.ReadLine()?.ToLower(), server);
+            }
+        }
+
+        static void Handler(string command, HttpServer server)
+        {
+            switch (command)
+            {
+                case "start":
+                    server.Start();
+                    break;
+                case "stop":
+                    server.Stop();
+                    break;
+                case "status":
+                    Console.WriteLine(server.Status);
+                    break;
+                case "restart":
+                    server.Stop();
+                    server.Start();
+                    break;
+                case "close":
+                    _isRunning = false;
+                    break;
+            }
+        }
+    }
+}
